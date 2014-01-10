97e38d97fd6de5bebe469cf83388a6c294809fb4
ZentetsuKenZ <zentetsukenz@gmail.com>
2014-01-11 02:09:32 +0700

    Initial commit
    - Complete part 2
    - Next is
    http://asanderson.org/posts/2013/08/19/bootstrapping-angular-rails-part-3.html

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '5e7bb743a78cf2225498ed83075063457656b95013f29604e76e7029b7cc1b59de5eae8c594931aa483cc438c255e86f781ffa92e617ade78d52f5aef0f32f3f'
